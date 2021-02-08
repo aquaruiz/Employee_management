81f5d9d555a8ca3185db40a8bd315fc0c2b23f62
aquaruiz <gpetiaa@gmail.com>
2021-02-08 13:00:22 +0200

    Revert "make service layer"
    This reverts commit 7d9f5696

--- src/main/java/io/manage/employee/repository/EmployeeRepository.java
+++ src/main/java/io/manage/employee/repository/EmployeeRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
-    void deleteEmployeeById(Long id);
 }
