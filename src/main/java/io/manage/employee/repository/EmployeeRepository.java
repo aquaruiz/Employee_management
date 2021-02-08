7d9f5696be7ea2692818938ae249499edd2895a6
aquaruiz <gpetiaa@gmail.com>
2021-02-08 12:49:11 +0200

    make service layer

--- src/main/java/io/manage/employee/repository/EmployeeRepository.java
+++ src/main/java/io/manage/employee/repository/EmployeeRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
 }
